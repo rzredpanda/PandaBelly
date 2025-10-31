2130000b7b8f8beedee294135abf9bd54ac77a65
Rr123454 <ritvin.rejith@gmail.com>
2025-10-31 14:05:39 -0700

    First Draft Private ArrayLists initialized

---
+++ Storage.java
@@ -0,0 +1,17 @@
+public class Storage{
+    private ArrayList<String> category;
+    private ArrayList<String> name;
+    private ArrayList<Double> price;
+    private ArrayList<Integer> amount;
+    private ArrayList<Integer> serialNumber;
+
+
+    public Storage(){
+        category = new ArrayList<String>();
+        name = new ArrayList<String>();
+        price = new ArrayList<Double>();
+        amount = new ArrayList<Integer>();
+        serialNumber = new ArrayList<Integer>();
+    }
+    
+}
