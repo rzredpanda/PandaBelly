09a9ffe656d186ebf9a58dd5b6977529012e397b
arthurW <arthur.yuxuan.wang@gmail.com>
2025-11-13 14:22:30 -0800

    Edited removeItem and addItem methods

--- Storage.java
+++ Storage.java
@@ -9,21 +9,36 @@ public class Storage {
         category = new ArrayList<Item>();
     }
 
-    public void removeItem(String n){
+    public boolean removeItem(String n){
         for (int i=0; i<category.size(); i++){
             if(category.get(i).getName().equals(n)){
                 category.remove(i);
+                return true;
             }
         }
+        return false;
     }
-    public void removeItem(int index){
-        for (int i=0; i<category.size(); i++){
-            if(i==index){
-                category.remove(i);
+    public boolean removeItem(int index){
+        if(index<=category.size()&&index>=0){
+            for (int i=0; i<category.size(); i++){
+                if(i==index){
+                    category.remove(i);
+                    return true;
+                }
             }
         }
+        return false;
     }
-    
+    public boolean addItem(String n, double p, int q, int s){
+        for(Item y:category){
+            if(y.getName().equals(n)){
+                return false;
+            }
+        }
+        category.add(new Item(n,p,q,s));
+        return true;
+    }
+
     
 }
 
