ae5822eaabe0ffa6e2c0ee0692455c56512a9fcc
arthurW <arthur.yuxuan.wang@gmail.com>
2025-11-17 13:43:39 -0800

    added setters to Item class

--- Item.java
+++ Item.java
@@ -2,9 +2,9 @@ public class Item {
     private String name;
     private double price;
     private int quantity;
-    private int SKU;
+    private String SKU;
 
-    public Item(String n, double p, int q, int s){
+    public Item(String n, double p, int q, String s){
         name=n;
         price=p;
         quantity=q;
@@ -19,7 +19,20 @@ public class Item {
     public int getQuantity(){
         return quantity;
     }
-    public int getSKU(){
+    public String getSKU(){
         return SKU;
+    }
+
+    public void setName(String n){
+        name=n;
+    }
+    public void setPrice(double x){
+        price=x;
+    }
+    public void setQuantity(int q){
+        quantity=q;
+    }
+    public void setSKU(String s){
+        SKU=s;
     }
 }
